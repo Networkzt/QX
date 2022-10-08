99dadca941a4ec88524b7150ca17f9e2dc483ab4
Networkevin <61804157+Networkzt@users.noreply.github.com>
2022-10-09 00:24:48 +0800

    Update wifi-changed.js

--- wifi-changed.js
+++ wifi-changed.js
@@ -1,4 +1,5 @@
-const WIFI_DONT_NEED_PROXYS = ['Home_5G', 'Home'];
+const WIFI_DONT_NEED_PROXYS = ['Home_5G'];
+const WIFI_DONT_NEED_PROXYS = ['Home'];
 const CURRENT_WIFI_SSID_KEY = 'current_wifi_ssid';
 
 if (wifiChanged()) {
